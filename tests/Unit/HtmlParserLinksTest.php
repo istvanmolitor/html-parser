29ca3e1f84ee2b5e1b7bf40303d1ae64a258e7f5
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-09-18 21:30:43 +0200

    fix tests

--- tests/Unit/HtmlParserLinksTest.php
+++ tests/Unit/HtmlParserLinksTest.php
@@ -13,4 +13,35 @@ class HtmlParserLinksTest extends TestCase
         $html = file_get_contents(__DIR__ . '/html/links.html');
         $this->parser = new HtmlParser($html);
     }
+
+    public function test_get_links(): void {
+        $links = [
+            [
+                'href' => '/relative',
+                'text' => 'Rel',
+            ],
+            [
+                'href' => 'http://example.com/abs',
+                'text' => 'Abs',
+            ],
+            [
+                'href' => '#',
+                'text' => 'Hash',
+            ],
+            [
+                'href' => 'mailto:test@example.com',
+                'text' => 'Mail',
+            ],
+            [
+                'href' => '/img/logo.png',
+                'text' => 'Image',
+            ],
+            [
+                'href' => '',
+                'text' => 'Space',
+            ],
+        ];
+
+        $this->assertSame($links, $this->parser->parseLinks());
+    }
 }
